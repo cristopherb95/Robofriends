31f8c1dad551ffce137a079f354625bfecbbf5c3
Krzysztof <cristopherb95@yahoo.com>
2020-01-22 11:21:33 +0100

    Added styling, cleaned up

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+
 const CardList = ({robots}) => {
 	const cardsArray = robots.map( (user) => {
 		return <Card key={user.id} id={user.id} name={user.name} email={user.email} />
---
+++ src/containers/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import './App.css'
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
+
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+
+    render() { 
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(rob => {
+            return rob.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        return (
+            <div className="tc">
+                <h1 className="f1">Domo arigato, Mr. Roboto</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        ); 
+    }
+}
+
+export default App;
